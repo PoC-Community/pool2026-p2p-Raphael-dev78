00affdaba35de9d6c0b17a884aaece3f572c83cf
raphael.dekeyser <raphael.dekeyser@epitech.eu>
2026-02-02 11:22:20 +0100

    variables et visibilite

--- day01-solidity/src/SmartContract.sol
+++ day01-solidity/src/SmartContract.sol
@@ -3,4 +3,12 @@ pragma solidity ^0.8.20;
 
 contract SmartContract {
 
+    uint256 public halfAnswerOfLife = 21;
+    address public myEthereumContractAddress = address(this);
+    address public myEthereumAddress = msg.sender;
+    string public PoCIsWhat = "PoC is good, PoC is life.";
+
+    bool internal _areYouABadPerson = false;
+
+    int256 private _youAreACheater = -42;
 }
