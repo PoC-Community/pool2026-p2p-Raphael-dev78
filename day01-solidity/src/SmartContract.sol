fd69a607520440e6b14ace4117def09720a2c93f
raphael.dekeyser <raphael.dekeyser@epitech.eu>
2026-02-02 11:41:18 +0100

    advanced types

--- day01-solidity/src/SmartContract.sol
+++ day01-solidity/src/SmartContract.sol
@@ -6,9 +6,39 @@ contract SmartContract {
     uint256 public halfAnswerOfLife = 21;
     address public myEthereumContractAddress = address(this);
     address public myEthereumAddress = msg.sender;
-    string public PoCIsWhat = "PoC is good, PoC is life.";
+    string public poCIsWhat = "PoC is good, PoC is life.";
 
     bool internal _areYouABadPerson = false;
 
     int256 private _youAreACheater = -42;
+
+    bytes32 whoIsTheBest;
+    mapping(string => uint256) public myGrades;
+    string[5] public myPhoneNumber;
+
+    enum RoleEnum { STUDENT, TEACHER }
+
+    struct Informations {
+        string firstName;
+        string lastName;
+        uint8 age;
+        string city;
+        RoleEnum role;
+    }
+
+    Informations public myInformations = Informations({
+        firstName: "Raphael",
+        lastName: "Unknown",
+        age: 25,
+        city: "Paris",
+        role: RoleEnum.STUDENT
+    });
+
+    Informations public myTeacher = Informations({
+        firstName: "Alice",
+        lastName: "Wonderland",
+        age: 30,
+        city: "London",
+        role: RoleEnum.TEACHER
+    });
 }
